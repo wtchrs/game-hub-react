ef145a4c20ebd3fe50f15b8473df6c4e38dc9225
wtchrs <wtchr_@hotmail.com>
2023-09-26 15:39:20 +0900

    Replace react logo with custom logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,11 @@
 import {HStack, Image} from '@chakra-ui/react';
-import reactLogo from '../assets/react.svg';
+import logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch.tsx';
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={reactLogo} boxSize="60px"/>
+      <Image src={logo} boxSize="60px"/>
       <ColorModeSwitch/>
     </HStack>
   );
